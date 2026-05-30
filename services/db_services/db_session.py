df64d6314ab150675ad33e2d400c777d71ddbb28
VBCreators <bhargav.vbcreators@gmail.com>
2026-05-30 16:44:42 +0530

    create DB connection

---
+++ services/db_services/db_session.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config import DATABASE_URL
+
+# Base class for all ORM models
+Base = declarative_base()
+
+# Create SQLAlchemy engine
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,  # Set True if you want to see SQL queries in terminal
+    future=True,
+)
+
+# Session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db_session():
+    # Create and return a new database session.
+    return SessionLocal()
